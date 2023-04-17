b928db9783cd223bd2256c04f3d5fa7cfc6c463e
philipperolet <philipperolet@gmail.com>
2023-04-17 16:05:21 +0200

    fixed pinecone test deleting db (!) + moved xp tests in specific folder

--- test/experiments/test_pinecone.py
+++ test/experiments/test_pinecone.py
@@ -7,6 +7,7 @@ from bip import utils
 # Connect to pinecone
 pinecone.init(api_key=utils.get_secret("pinecone"),
               environment="eu-west1-gcp")
+test_namespace = "initial-pinecone-test"
 
 # get 2 embeddings
 embeddings = OpenAIEmbeddings()
@@ -15,14 +16,16 @@ elt2 = embeddings.embed_query(text="Il est très en retard")
 
 # Insert some data
 index = pinecone.Index("bip-email")
-index.delete(deleteAll=True)
-index.upsert(vectors=[("elt0", [0.1]*1536)], namespace="test")
-index.upsert(vectors=[("elt1", elt1, { "m_id": "wow"}),
-                      ("elt2", elt2, { "m_id": "wow2"})], namespace="test")
+index.delete(deleteAll=True, namespace=test_namespace)
+index.upsert(vectors=[("elt0", [0.1]*1536)], namespace=test_namespace)
+index.upsert(vectors=[("elt1", elt1, {"m_id": "wow"}),
+                      ("elt2", elt2, {"m_id": "wow2"})],
+             namespace=test_namespace)
 
 # Check it works
 query1 = embeddings.embed_query(text="Une personne de mon équipe")
 query2 = embeddings.embed_query(text="Elle est à l'heure")
-print(index.fetch(ids=["elt0"], namespace="test")['vectors']['elt0']['values'][:5])
-print(index.query(top_k=1, vector=query1, namespace="test"))
-print(index.query(top_k=1, vector=query2, namespace="test"))
+print(index.fetch(ids=["elt0"],
+                  namespace=test_namespace)['vectors']['elt0']['values'][:5])
+print(index.query(top_k=1, vector=query1, namespace=test_namespace))
+print(index.query(top_k=1, vector=query2, namespace=test_namespace))
