a008a2cdd8d9c1df32a77722cb9f795f533dafc9
philipperolet <philipperolet@gmail.com>
2023-04-22 07:35:03 +0200

    modified index config

--- db.py
+++ db.py
@@ -5,16 +5,11 @@ from bip import utils
 pinecone.init(api_key=utils.get_secret("pinecone"),
               environment="eu-west1-gcp")
 
-pinecone.create_index('emails', dimension=1536,
-                      metric='cosine',
-                      pods=1,
-                      replicas=1,
-                      pod_type='p1.x1')
 metadata_config = {
-    'indexed': ['subject', 'date', 'thread_id']
+    'indexed': ['subject', 'date', 'thread_id', 'message_id']
 }
-pinecone.create_index('emails', dimension=1536,
-                      metric='cosine',
+pinecone.create_index('emails-euclidean', dimension=1536,
+                      metric='euclidean',
                       pods=1,
                       replicas=1,
                       pod_type='p1.x1',
